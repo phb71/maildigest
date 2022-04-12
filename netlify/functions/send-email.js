3dbf1cae53a2a3a401c65aa999f2d05a55631ce6
Paul-Henri Boudet <paul@Paul-Henris-MacBook-Pro.local>
2022-04-12 16:38:13 +0200

    adding weather temp in netlify function + user registration form part 1

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,25 +1,41 @@
-let apiKey = process.env.POSTMARK_API_TOKEN;
+
+const axios = require('axios');
+
+let postmarkKey = process.env.POSTMARK_API_TOKEN;
+let openweatherKey = process.env.OPENWEATHER_API_TOKEN;
+
 let postmark = require("postmark")
-let client = new postmark.ServerClient(apiKey);
+let emailClient = new postmark.ServerClient(postmarkKey);
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const d = new Date();
 const dayName = days[d.getDay()];
 
-exports.handler = (event, context, callback) => {
-    client.sendEmail(
-        {
-            From: "info@urban-hideout.com",
-            To: "paul@urban-hideout.com",
-            Subject: dayName + ' digest',
-            HtmlBody: "test",
-            TextBody: "test"
-        }
-    ).then(response => {
-        console.log(response.To);
-        console.log(response.SubmittedAt);
-        console.log(response.Message);
-        console.log(response.MessageID);
-        console.log(response.ErrorCode);
-    });
+// Using Async
+exports.handler = async function (event, context) {
+    
+    const weatherTemp = await axios.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=46.6013&lon=4.0650&appid='+ openweatherKey + '&units=metric'
+      );
+
+    const emailSend = await emailClient.sendEmail({
+        From: "info@urban-hideout.com",
+        To: "paul@urban-hideout.com",
+        Subject: dayName + ' digest',
+        // HtmlBody: weatherTemp.data.main.temp,
+        HtmlBody: "a",
+        TextBody: "test"
+    })
+
+    console.log("Response:");
+    console.log(emailSend.To);
+    console.log(emailSend.SubmittedAt);
+    console.log(emailSend.Message);
+    console.log(emailSend.MessageID);
+    console.log(emailSend.ErrorCode);
+    
+    return {
+        statusCode: 200,
+      };
+   
 }
---
+++ src/components/CreateAccount.vue
@@ -0,0 +1,47 @@
+<template>
+  <p>
+    <label
+      >Email
+      <input type="text" v-model="email" required />
+    </label>
+  </p>
+  <p>
+    <label
+      >Password
+      <input type="password" v-model="password" required />
+    </label>
+  </p>
+  <button @click="createAccount">Sign me up!</button>
+  <div class="message"></div>
+</template>
+<script>
+import GoTrue from "gotrue-js";
+
+const auth = new GoTrue({
+  APIUrl:
+    "https://imaginative-sfogliatella-76a713.netlify.app/.netlify/identity",
+  audience: "",
+  setCookie: false,
+});
+
+export default {
+  name: "CreateAccount",
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+
+  mounted() {},
+
+  methods: {
+    createAccount() {
+      auth
+        .signup(this.email, this.password)
+        .then((response) => console.log("Success", response))
+        .catch((error) => console.log("It 's an error", error));
+    },
+  },
+};
+</script>
