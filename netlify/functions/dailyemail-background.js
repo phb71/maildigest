592205bf07b33caa5e17277a1c7e81fd9ed6f7af
Paul <ph.boudet@gmail.com>
2022-07-12 12:22:15 +0700

    testing background function

--- netlify/functions/dailyemail-background.js
+++ netlify/functions/dailyemail-background.js
@@ -1,25 +1,31 @@
 const axios = require('axios')
 
 // TODO
-// 1 Get the list of users
+// 1 Get the list of users and their meta data
 // 2 Call the send-email function for each
-// 3 Get the meta data for each user to populate the email
 // 4 Run it as a cron
 
 exports.handler = function (event, context) {
-  try {
-    const response = axios.post(
-      'http://localhost:8888/.netlify/functions/send-email?city=' +
-          'paris' +
-          '&lat=' +
-          '10' +
-          '&lon=' +
-          '20' +
-          '&email=' +
-          'paul@urban-hideout.com'
-    )
-    console.log(response)
-  } catch (error) {
-    console.log(error)
-  }
+  // Get the list of users and their meta data
+  const users = axios.get(
+    'http://localhost:8888/.netlify/functions/list-users'
+  )
+  console.log(users)
+
+  // Send the email
+//   try {
+//     const response = axios.post(
+//       'http://localhost:8888/.netlify/functions/send-email?city=' +
+//           'paris' +
+//           '&lat=' +
+//           '10' +
+//           '&lon=' +
+//           '20' +
+//           '&email=' +
+//           'paul@urban-hideout.com'
+//     )
+//     console.log(response)
+//   } catch (error) {
+//     console.log(error)
+//   }
 }
