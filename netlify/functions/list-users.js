094c9ddb90459445c2adbdd14cd747a69b597e05
Paul <ph.boudet@gmail.com>
2022-07-12 12:37:46 +0700

    fix

--- netlify/functions/list-users.js
+++ netlify/functions/list-users.js
@@ -27,6 +27,6 @@ exports.handler = async (event, context) => {
   /* Returning the userEmail variable to the front end. */
   return {
     statusCode: 200,
-    body: JSON.stringify(response)
+    body: String(userEmail)
   }
 }
