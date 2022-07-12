e3741985965a81a22a69d503014cd40517119aec
Paul <ph.boudet@gmail.com>
2022-07-12 19:34:28 +0700

    fix

--- netlify/functions/dailyemailbg.js
+++ netlify/functions/dailyemailbg.js
@@ -4,8 +4,9 @@ const axios = require('axios')
 // TODO
 // 1 Get the list of users and their meta data ✅
 // 2 Populate the email content ???
-// 3 Call the send-email function for each ✔️
-// 4 Run it as a cron ✔️
+// 3 Call the send-email function for each
+// 4 Run it as a cron
+// 5 Turn these 2 functions into utilities
 
 exports.handler = async function (event, context) {
   let users
@@ -28,14 +29,14 @@ exports.handler = async function (event, context) {
   }
 
   /* Loop through the users array and adds the email and city to the result variable. */
-  users.forEach(element => {
+  for(const element of users) {
     let response
     result += element.email + (element.metadata.city ? ', living in ' + element.metadata.city.name + '\n' : '\n')
     // Send the email
     try {
       if (element.metadata.city) {
         console.log('Sending email to ' + element.email + ' in ' + element.metadata.city.name)
-        response = axios.post(
+        response = await axios.post(
           'https://imaginative-sfogliatella-76a713.netlify.app/.netlify/functions/send-email?city=' +
             element.metadata.city.name +
             '&lat=' +
@@ -47,7 +48,7 @@ exports.handler = async function (event, context) {
         )
       } else {
         console.log('Sending email to ' + element.email + ' when no metadata found')
-        response = axios.post(
+        response = await axios.post(
           'https://imaginative-sfogliatella-76a713.netlify.app/.netlify/functions/send-email?city=none&lat=1&lon=1&email=' +
             element.email
         )
