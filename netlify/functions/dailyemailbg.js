8742ff744a9ca7b8d154ed3626f6d260a135668a
Paul <ph.boudet@gmail.com>
2022-07-14 13:57:25 +0700

    fix

--- netlify/functions/dailyemailbg.js
+++ netlify/functions/dailyemailbg.js
@@ -15,36 +15,38 @@ exports.handler = async function (event, context) {
   const users = await getUsers(context)
   console.log(users)
   /* Loop through the users array and adds the email and city to the result variable. */
-  // for (const element of users) {
-  //   let response
-  //   // result += element.email + (element.metadata.city ? ', living in ' + element.metadata.city.name + '\n' : '\n')
-  //   // Send the email
-  //   // try {
-  //   //   if (element.metadata.city) {
-  //   //     console.log('Sending email to ' + element.email + ' in ' + element.metadata.city.name)
-  //   //     response = await axios.post(
-  //   //       'https://imaginative-sfogliatella-76a713.netlify.app/.netlify/functions/send-email?city=' +
-  //   //         element.metadata.city.name +
-  //   //         '&lat=' +
-  //   //         element.metadata.city.lat +
-  //   //         '&lon=' +
-  //   //         element.metadata.city.lon +
-  //   //         '&email=' +
-  //   //         element.email
-  //   //     )
-  //   //   } else {
-  //   //     console.log('Sending email to ' + element.email + ' when no metadata found')
-  //   //     response = await axios.post(
-  //   //       'https://imaginative-sfogliatella-76a713.netlify.app/.netlify/functions/send-email?city=none&lat=1&lon=1&email=' +
-  //   //         element.email
-  //   //     )
-  //   //   }
-  //   //   console.log('Email sent: ' + JSON.stringify(response))
-  //   // } catch (error) {
-  //   //   console.log('Error: ' + error)
-  //   // }
-  // }
-
+  for (const element of users) {
+    let response
+    console.log(element.email)
+    console.log(element.metadata.city.name)
+    // result += element.email + (element.metadata.city ? ', living in ' + element.metadata.city.name + '\n' : '\n')
+    // Send the email
+    // try {
+    //   if (element.metadata.city) {
+    //     console.log('Sending email to ' + element.email + ' in ' + element.metadata.city.name)
+    //     response = await axios.post(
+    //       'https://imaginative-sfogliatella-76a713.netlify.app/.netlify/functions/send-email?city=' +
+    //         element.metadata.city.name +
+    //         '&lat=' +
+    //         element.metadata.city.lat +
+    //         '&lon=' +
+    //         element.metadata.city.lon +
+    //         '&email=' +
+    //         element.email
+    //     )
+    //   } else {
+    //     console.log('Sending email to ' + element.email + ' when no metadata found')
+    //     response = await axios.post(
+    //       'https://imaginative-sfogliatella-76a713.netlify.app/.netlify/functions/send-email?city=none&lat=1&lon=1&email=' +
+    //         element.email
+    //     )
+    //   }
+    //   console.log('Email sent: ' + JSON.stringify(response))
+    // } catch (error) {
+    //   console.log('Error: ' + error)
+    // }
+  }
+console.log('the end')
   return {
     statusCode: 200,
     body: result
